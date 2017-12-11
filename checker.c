9edc76a315ef81c3ee72f412ed44b4f68778bba3
Jiri Svoboda <jiri@wiwaxia>
2017-12-11 20:06:07 +0100

    Let checker lex and parse the input.

--- checker.c
+++ checker.c
@@ -2,11 +2,75 @@
  * Checker
  */
 
+#include <adt/list.h>
 #include <assert.h>
+#include <ast.h>
 #include <checker.h>
 #include <lexer.h>
 #include <merrno.h>
+#include <parser.h>
+#include <stdbool.h>
 #include <stdlib.h>
+
+static void checker_parser_get_tok(void *, lexer_tok_t *);
+
+static parser_input_ops_t checker_parser_input = {
+	.get_tok = checker_parser_get_tok
+};
+
+/** Create checker module.
+ *
+ * @param input_ops Input ops
+ * @param input_arg Argument to input_ops
+ * @param rmodule Place to store new checker module.
+ *
+ * @return EOK on success, ENOMEM if out of memory
+ */
+static int checker_module_create(checker_module_t **rmodule)
+{
+	checker_module_t *module = NULL;
+
+	module = calloc(1, sizeof(checker_module_t));
+	if (module == NULL)
+		return ENOMEM;
+
+	list_initialize(&module->toks);
+
+	*rmodule = module;
+	return EOK;
+}
+
+/** Destroy checker module.
+ *
+ * @param checker Checker
+ */
+static void checker_module_destroy(checker_module_t *module)
+{
+	free(module);
+}
+
+/** Append a token to checker module.
+ *
+ * @param module Checker module
+ * @param tok Lexer token
+ *
+ * @return EOK on success, ENOMEM if out of memory
+ */
+static int checker_module_append(checker_module_t *module, lexer_tok_t *tok)
+{
+	checker_tok_t *ctok;
+
+	ctok = calloc(1, sizeof(checker_tok_t));
+	if (ctok == NULL)
+		return ENOMEM;
+
+	printf("\nappending: ");
+	(void) lexer_dprint_tok(tok, stdout);
+	ctok->mod = module;
+	ctok->tok = *tok;
+	list_append(&ctok->ltoks, &module->toks);
+	return EOK;
+}
 
 /** Create checker.
  *
@@ -36,7 +100,6 @@ int checker_create(lexer_input_ops_t *input_ops, void *input_arg,
 	}
 
 	checker->lexer = lexer;
-
 	*rchecker = checker;
 	return EOK;
 error:
@@ -53,8 +116,120 @@ error:
  */
 void checker_destroy(checker_t *checker)
 {
+	checker_module_destroy(checker->mod);
 	lexer_destroy(checker->lexer);
 	free(checker);
+}
+
+/** Lex a module.
+ *
+ * @param checker Checker
+ * @param rmodule Place to store pointer to new module
+ * @return EOK on success, or error code
+ */
+static int checker_module_lex(checker_t *checker, checker_module_t **rmodule)
+{
+	checker_module_t *module = NULL;
+	bool done;
+	lexer_tok_t tok;
+	int rc;
+
+	rc = checker_module_create(&module);
+	if (rc != EOK) {
+		assert(rc == ENOMEM);
+		goto error;
+	}
+
+	done = false;
+	while (!done) {
+		rc = lexer_get_tok(checker->lexer, &tok);
+		if (rc != EOK)
+			return rc;
+
+		rc = checker_module_append(module, &tok);
+		if (rc != EOK) {
+			lexer_free_tok(&tok);
+			return rc;
+		}
+
+		if (tok.ttype == ltt_eof)
+			done = true;
+	}
+
+	*rmodule = module;
+	return EOK;
+error:
+	return rc;
+}
+
+/** Get first token in a checker module.
+ *
+ * @param mod Checker module
+ * @return First token or @c NULL if the list is empty
+ */
+static checker_tok_t *checker_module_first_tok(checker_module_t *mod)
+{
+	link_t *link;
+
+	link = list_first(&mod->toks);
+	if (link == NULL)
+		return NULL;
+
+	return list_get_instance(link, checker_tok_t, ltoks);
+}
+
+/** Get next token in a checker module
+ *
+ * @param tok Current token or @c NULL
+ * @return Next token or @c NULL
+ */
+static checker_tok_t *checker_module_next_tok(checker_tok_t *tok)
+{
+	link_t *link;
+
+	if (tok == NULL)
+		return NULL;
+
+	link = list_next(&tok->ltoks, &tok->mod->toks);
+	if (link == NULL)
+		return NULL;
+
+	return list_get_instance(link, checker_tok_t, ltoks);
+}
+
+/** Parse a module.
+ *
+ * @param mod Checker module
+ * @return EOK on sucecss or error code
+ */
+static int checker_module_parse(checker_module_t *mod)
+{
+	ast_module_t *amod;
+	parser_t *parser;
+	checker_parser_input_t pinput;
+	int rc;
+
+	pinput.tok = checker_module_first_tok(mod);
+	rc = parser_create(&checker_parser_input, &pinput, &parser);
+	if (rc != EOK)
+		return rc;
+
+	rc = parser_process_module(parser, &amod);
+	if (rc != EOK)
+		return rc;
+
+	putchar('\n');
+
+	rc = ast_tree_print(&amod->node, stdout);
+	if (rc != EOK)
+		return rc;
+
+	putchar('\n');
+
+	mod->ast = amod;
+	parser_destroy(parser);
+
+	return EOK;
 }
 
 /** Run checker.
@@ -63,6 +238,31 @@ void checker_destroy(checker_t *checker)
  */
 int checker_run(checker_t *checker)
 {
-	(void)checker;
-	return 0;
+	int rc;
+
+	if (checker->mod == NULL) {
+		rc = checker_module_lex(checker, &checker->mod);
+		if (rc != EOK)
+			return rc;
+
+		rc = checker_module_parse(checker->mod);
+		if (rc != EOK)
+			return rc;
+	}
+
+	return EOK;
+}
+
+/** Parser input from checker token list.
+ *
+ * @param arg Checker parser input (checker_parser_input_t)
+ * @param tok Place to store token
+ */
+static void checker_parser_get_tok(void *arg, lexer_tok_t *tok)
+{
+	checker_parser_input_t *pinput = (checker_parser_input_t *)arg;
+
+	*tok = pinput->tok->tok;
+	if (tok->ttype != ltt_eof)
+		pinput->tok = checker_module_next_tok(pinput->tok);
 }
--- types/checker.h
+++ types/checker.h
@@ -5,12 +5,38 @@
 #ifndef TYPES_CHECKER_H
 #define TYPES_CHECKER_H
 
+#include <adt/list.h>
+#include <types/ast.h>
 #include <types/lexer.h>
+
+/** Checker token */
+typedef struct {
+	/** Containing checker module */
+	struct checker_module *mod;
+	/** Link in list of tokens */
+	link_t ltoks;
+	/** Lexer token */
+	lexer_tok_t tok;
+} checker_tok_t;
+
+/** Checker module */
+typedef struct checker_module {
+	/** Tokens */
+	list_t toks; /* of checker_tok_t */
+	ast_module_t *ast;
+} checker_module_t;
 
 /** Checker */
 typedef struct {
 	/** Lexer */
 	lexer_t *lexer;
+	/** Module */
+	checker_module_t *mod;
 } checker_t;
+
+/** Checker parser input */
+typedef struct {
+	checker_tok_t *tok;
+} checker_parser_input_t;
 
 #endif
