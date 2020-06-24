0baed5a1d5d7c8f649fb2ea960a0936857892237
Jiri Svoboda <jiri@wiwaxia>
2020-06-24 20:23:45 +0200

    Add some tests for preprocessor macros

---
+++ test/good/preproc-in.c
@@ -0,0 +1,10 @@
+/*
+ * Preprocessor directives should be accepted.
+ * (They are only checked for leading indentation, otherwise ignored)
+ */
+
+#include <stdio.h>
+
+#ifndef FOO
+#define FOO
+#endif
---
+++ test/ugly/preproc-fixed.c
@@ -0,0 +1,14 @@
+/*
+ * Preprocessor directives should not have any indentation
+ */
+
+#include <stdio.h>
+
+#ifndef FOO
+#define FOO
+#include <foostuff.h>
+#endif
+
+#ifndef BAR
+#define BAR
+#endif
